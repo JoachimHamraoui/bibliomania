194b289a0062d8fa1991d03b6c0bdfefa7db4a96
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2024-04-01 15:12:49 +0200

    feat(image upload): tested image uploading with firebase and nodejs, retrieve downloadurl from stored image in Firebase storage and post this to a PostgreSQL database. Will be reused for other purposes in project.

--- images/api/src/server.js
+++ images/api/src/server.js
@@ -3,15 +3,39 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const db = require("./database/connect_database");
 // Middlewares
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(cors());
 
-
 app.get("/", (req,res ) =>{
     res.send("taboundimek")
   })
+
+ 
+
+// Define a route for image upload
+app.post('/upload', async (req, res) => {
+  const {id, image_url, created_at, updated_at  } = req.body;
+
+  try {
+    await db('images').insert({
+      id, image_url, created_at, updated_at
+    })
+
+    res.status(201).send({
+      data: { id, image_url, created_at, updated_at },
+      message: 'Added successfully'
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error: 'Something went wrong',
+      value: error
+    });
+  }
+});
+
 
 module.exports = app;
