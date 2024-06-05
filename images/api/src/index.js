8c2712c2e668aa177504eedcbd1076c4bed60993
Joachim Hamraoui <56119451+JoachimHamraoui@users.noreply.github.com>
2024-06-05 20:54:59 +0200

    Update index.js

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -3,10 +3,6 @@ require("dotenv").config();
 const app = require("./server");
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
 app.listen(port, () => {
   console.log(`Server is running on ports ${port}`);
 });
